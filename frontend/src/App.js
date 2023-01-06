4d49404d60ebc718f6e187b6fc51ef3267fab196
Atif Hussain <atifhussain28@gmail.com>
2023-01-07 00:17:05 +0530

    Added marker to mapbox successfully.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import {Map,Marker} from 'react-map-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+import {Room} from "@material-ui/icons"
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 20.5937,
+    longitude:78.9629,
+    zoom: 5,
+  });
+
   return (
-    <div className="App">
-     hello
+    <div className="App" style={{ height: "100vh", width: "100%" }}>
+     <Map
+    {...viewport}
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    onMove={viewport => setViewport(viewport)}
+    mapStyle="mapbox://styles/mapbox/streets-v9">
+      
+
+       <Marker latitude={27.1751} longitude={78.0421}   >
+        <Room></Room>
+     </Marker>
+    </Map>
+    ;
     </div>
   );
 }
